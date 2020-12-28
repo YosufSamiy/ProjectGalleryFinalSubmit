2a8552c06974b189b75bb12f0b662288b8647860
YosufSamiy <m77mm1999@gmail.com>
2020-12-29 00:38:04 +0200

    first commit

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -30,7 +30,8 @@ class LoginController extends Controller
 
     /**
      * Create a new controller instance.
-     *
+     *php artisan cache:clear
+
      * @return void
      */
     public function __construct()
